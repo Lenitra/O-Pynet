23e15fcafaa663d4389dc1cb83ee73561d4d4f79
Lenitra <lenitramc@gmail.com>
2022-03-21 15:00:10 +0100

    Setup in one command

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@
 # Get crontab
 crontab -l > mycron
 # Append new cron into cron file
-echo "@reboot bash O-Pynet/src/start.sh" >> mycron
+echo "@reboot bash /home/ubuntu/O-Pynet/src/start.sh" >> mycron
 # Install new cron file
 crontab mycron
 rm mycron
