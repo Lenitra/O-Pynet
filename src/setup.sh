862695eb570c50d1b9e022a4a9b5915ef334beb2
Lenitra <lenitramc@gmail.com>
2022-04-10 00:50:36 +0200

    loop.sh avec installation via le setup

--- src/setup.sh
+++ src/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# add "@reboot bash O-Pynet/src/start.sh" to crontab
-# Get crontab
 crontab -l > mycron
-# Append new cron into cron file
 echo "@reboot bash /home/ubuntu/O-Pynet/src/start.sh" >> mycron
-# Install new cron file
 crontab mycron
 rm mycron
+
+crontab -l > mycron
+echo "@reboot bash /home/ubuntu/O-Pynet/src/loop.sh" >> mycron
+crontab mycron
+rm mycron
+
 sudo reboot
