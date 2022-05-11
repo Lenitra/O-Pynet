8f31f31c44ac0e574c6bc5677d6c9eaea570e8dc
Lenitra <lenitramc@gmail.com>
2022-05-12 01:15:08 +0200

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 import time
 
+os.system("cd..;pwd")
+os.system("pwd")
 os.system("cd ..;sudo rm -rf O-Pynet")
 os.system("cd ..;git clone https://github.com/Lenitra/O-Pynet.git")
 time.sleep(2)
