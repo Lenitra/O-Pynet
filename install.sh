809b9175022c30b568022bf5ddf025b8d54d4d08
TauMah <lenitramc@gmail.com>
2023-07-22 00:06:07 +0200

    Install crontab pas correct

--- install.sh
+++ install.sh
@@ -1,16 +1,4 @@
 # Description: Install O-Pynet
 
 
-cd ~
-
-sudo rm -rf O-Pynet
-git clone https://github.com/Lenitra/O-Pynet.git
-
-cd O-Pynet
-
-sudo pip install -r requirements.txt
-pip install -r requirements.txt
-
-# add to the crontab the command : @reboot sudo python3 /home/O-Pynet/main.py
-
-crontab -l | { cat; echo "@reboot bash ~/O-Pynet/start.sh &"; } | crontab -
+cd ~;sudo rm -rf O-Pynet;git clone https://github.com/Lenitra/O-Pynet.git;cd O-Pynet;sudo pip install -r requirements.txt;pip install -r requirements.txt;crontab -l | { cat; echo "@reboot cd ~/O-Pynet;bash ~/O-Pynet/start.sh &"; } | crontab -;
