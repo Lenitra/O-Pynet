e5d07d1e4cee16e38b18c0a5daf1903b6b4c9e77
TauMah <lenitramc@gmail.com>
2023-07-23 13:14:24 +0200

    Maj automatique

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 
 cd ~
+# copy the O-Pynet conf folder to the home directory named as 555temp555
+cp -r O-Pynet/conf 555temp555
+# remove the O-Pynet folder
+rm -rf O-Pynet
+# clone the O-Pynet repository
+git clone https://github.com/Lenitra/O-Pynet.git
+
+# copy the conf folder to the O-Pynet folder
+cp -r 555temp555 O-Pynet/conf
+# remove the 555temp555 folder
+rm -rf 555temp555
+
 cd O-Pynet
 sudo python3 main.py
