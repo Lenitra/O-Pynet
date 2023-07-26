6b24d206a04e2298a4196914b387fd584dd141c8
TauMah <lenitramc@gmail.com>
2023-07-26 12:08:24 +0200

    Faute de fappe

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-# Description: Install O-Pynet
-
-
-cd ~;sudo rm -rf O-Pynet;git clone https://github.com/Lenitra/O-Pynet.git;cd O-Pynet;sudo pip install -r requirements.txt;pip install -r requirements.txt;crontab -l | { cat; echo "@reboot cd ~/O-Pynet;bash start.sh &"; } | crontab -; mkidr ~/photos;sudo reboot;
+cd ~;sudo rm -rf O-Pynet;git clone https://github.com/Lenitra/O-Pynet.git;cd O-Pynet;sudo pip install -r requirements.txt;pip install -r requirements.txt;crontab -l | { cat; echo "@reboot cd ~/O-Pynet;bash start.sh &"; } | crontab -; mkdir ~/photos;sudo reboot;
