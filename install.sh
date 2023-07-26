84cc8ca23a609a63adc3e54927a7ecdfe9b1ba25
TauMah <lenitramc@gmail.com>
2023-07-26 12:13:39 +0200

    ?

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-cd ~;sudo rm -rf O-Pynet;git clone https://github.com/Lenitra/O-Pynet.git;cd O-Pynet;sudo pip install -r requirements.txt;pip install -r requirements.txt;crontab -l | { cat; echo "@reboot cd ~/O-Pynet;bash start.sh &"; } | crontab -; mkdir ~/photos;sudo reboot;
+cd ~;
+sudo rm -rf O-Pynet;
+git clone https://github.com/Lenitra/O-Pynet.git;
+cd O-Pynet;
+sudo pip install -r ~/O-Pynet/requirements.txt;
+crontab -l | { cat; echo "@reboot cd ~/O-Pynet;bash start.sh &"; } | crontab -;
+mkdir ~/photos;
+sudo reboot;
