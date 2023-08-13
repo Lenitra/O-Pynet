2ecd8b64c2dca9b34e7acbe7b31208f954040e75
Thomas <lenitramc@gmail.com>
2023-08-13 11:07:23 +0200

    Start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
 
 cd ~
 # copy the O-Pynet conf folder to the home directory named as 555temp555
-# cp -r O-Pynet/conf 555temp555
+cp -r O-Pynet/conf 555temp555
 # copy this file to the home directory named as 555temp555.sh
 # cp O-Pynet/start.sh 555temp555.sh
 # run the 555temp555.sh file
 # bash 555temp555.sh &
 # remove the O-Pynet folder
-# rm -rf O-Pynet
+rm -rf O-Pynet
 # clone the O-Pynet repository
-# git clone https://github.com/Lenitra/O-Pynet.git
+git clone https://github.com/Lenitra/O-Pynet.git
 
 # copy the conf folder to the O-Pynet folder
-# cp -r 555temp555 O-Pynet/conf
+rm -rf O-Pynet/conf
+cp -r 555temp555 O-Pynet/conf
 # remove the 555temp555 folder
-# rm -rf 555temp555
+rm -rf 555temp555
 
 cd O-Pynet
 sudo python3 main.py
