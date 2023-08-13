2d8b978a4708014d47dfd4d694a58835255ded12
Thomas <lenitramc@gmail.com>
2023-08-13 15:36:39 +0200

    Style base pour photos

--- static/commun.js
+++ static/commun.js
@@ -7,7 +7,7 @@ function menu(title){
         <ul>
             <li><a href="/dashboard">Accueil</a></li>
             <li><a href="/config">Paramètres</a></li>
-            <li><a href="/photos">Photos (SoOn)</a></li>
+            <li><a href="/photos">Photos</a></li>
             <li><a href="/logs">Logs (SoOn)</a></li>
         </ul>
     </div>
