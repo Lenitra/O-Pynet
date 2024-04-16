5a49a51518ed25da6d24d2b0158e08a9ee87c9f4
Lenitra <lenitramc@gmail.com>
2024-04-16 17:14:53 +0200

    Move torun.bat and execute it in a new window

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
 @REM move config.json to ../config.json
 cd ..
 move O-Pynet/config.json /tempopyconfig.json
-@REM delete O-Pynet folder
-rmdir /s /q O-Pynet
-git clone https://github.com/Lenitra/O-Pynet.git
-move /tempopyconfig.json O-Pynet/config.json
-cd O-Pynet
-python main.py
+move O-Pynet/torun.bat /torun.bat
+@REM execute torun.bat in a new window
+start torun.bat
+
---
+++ torun.bat
@@ -0,0 +1,5 @@
+rmdir /s /q O-Pynet
+git clone https://github.com/Lenitra/O-Pynet.git
+move /tempopyconfig.json O-Pynet/config.json
+cd O-Pynet
+python main.py
