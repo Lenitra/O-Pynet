3b90ae19c092d5ab19806fca0be29b71c6e67052
Lenitra <lenitramc@gmail.com>
2024-04-16 17:21:20 +0200

    Comment torun.bat

--- torun.bat
+++ torun.bat
@@ -1,5 +1,14 @@
+@REM Supprime le répertoire O-Pynet
 rmdir /s /q O-Pynet
+
+@REM Installe la dernière version de O-Pynet
 git clone https://github.com/Lenitra/O-Pynet.git
+
+@REM Récupère l'ancien fichier de configuration
 move /tempopyconfig.json O-Pynet/config.json
+
+@REM Passe sur le répertoire O-Pynet
 cd O-Pynet
+
+@REM Exécute le programme
 python main.py
