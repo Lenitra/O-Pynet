bca6b46c8172cba23619854f9050c0d671e68f6f
Lenitra <lenitramc@gmail.com>
2024-04-16 17:08:32 +0200

    Update start.bat script to move and clone O-Pynet repository

--- start.bat
+++ start.bat
@@ -1,1 +1,9 @@
+@REM move config.json to ../config.json
+cd ..
+move O-Pynet/config.json /tempopyconfig.json
+@REM delete O-Pynet folder
+rmdir /s /q O-Pynet
+git clone https://github.com/Lenitra/O-Pynet.git
+move /tempopyconfig.json O-Pynet/config.json
+cd O-Pynet
 python main.py
