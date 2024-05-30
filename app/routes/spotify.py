dbd6a36a6c360975157abc6d8a7af8efe31bc97b
Lenitra <lenitramc@gmail.com>
2024-05-30 14:05:37 +0200

    Liaison de l'api spotify

---
+++ app/routes/spotify.py
@@ -0,0 +1,89 @@
+import base64
+import time
+import webbrowser
+from flask import Blueprint, render_template, session, redirect, request, jsonify
+import json
+
+import requests
+
+with open('access_token.txt', 'w') as f:
+    f.write('')
+    
+SPOTIFY = Blueprint('spotify', __name__)
+
+@SPOTIFY.route('/spotify')
+def spotify():
+    if 'user' not in session:
+        return redirect("/login")
+    return render_template("spotify.html")
+
+# Fonction pour obtenir le jeton d'accès en échange du code d'autorisation
+def get_access_token(code):
+    with open('config.json') as f:
+        config = json.load(f)
+    client_id = config['spotify']['client_id']
+    client_secret = config['spotify']['client_secret']
+    token_url = 'https://accounts.spotify.com/api/token'
+    headers = {'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode()}
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': "http://" + config["host"]+ ":" + config["port"] + '/spotify/callback',
+    }
+    response = requests.post(token_url, headers=headers, data=data)
+    token_info = response.json()
+    return token_info.get('access_token')
+
+
+
+@SPOTIFY.route('/spotify/callback')
+def callback():
+    code = request.args.get('code')
+    print("Le code expirera dans : ", request.args.get('expires_in'), " secondes")
+    access_token = get_access_token(code)
+    # save the access_token in a file
+    with open('access_token.txt', 'w') as f:
+        f.write(access_token)
+    if access_token:
+        # Vous pouvez effectuer d'autres actions ici, comme enregistrer le jeton d'accès dans une base de données
+        return "Jetons d'accès obtenu avec succès!<br>"+access_token+"<script>setTimeout(function(){window.close();}, 1);</script>"
+    else:
+        return "Impossible d'obtenir le jeton d'accès."
+    
+    
+    
+@SPOTIFY.route('/spotify/getkey', methods=['GET' , 'POST'])
+def login():
+    with open('config.json') as f:
+        config = json.load(f)
+    client_id = config['spotify']['client_id']
+    redirect_uri = "http://" + config["host"]+ ":" + config["port"] + '/spotify/callback'
+    # Remplacez les scopes par ceux que vous souhaitez demander à l'utilisateur
+    scope = 'user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming '
+    state = 'some_state_value'  # Vous pouvez générer un état aléatoire ici si nécessaire
+    authorize_url = 'https://accounts.spotify.com/authorize?' + \
+                    'response_type=code&client_id=' + client_id + \
+                    '&scope=' + scope + \
+                    '&redirect_uri=' + redirect_uri + \
+                    '&state=' + state
+    return redirect(authorize_url)
+    
+    
+    
+@SPOTIFY.route('/addqueue', methods=['POST'])
+def addqueue():
+    # Récupérer le jeton d'accès
+    with open('access_token.txt', 'r') as f:
+        access_token = f.read()
+    with open('config.json') as f:
+        config = json.load(f)
+    headers = {'Authorization': 'Bearer ' + access_token}
+    uri = request.args.get('uri')
+    response = requests.post(f'https://api.spotify.com/v1/me/player/queue?uri={uri}', headers=headers)
+    if response.status_code != 204:
+        webbrowser.open('http://'+config["host"]+':'+config["port"]+'/spotify/getkey')
+        time.sleep(3)
+        requests.post(f'http://' + config["host"] + ':' + config["port"] + '/addqueue?uri=' + uri)
+    return "OK", 200
+
+
---
+++ app/static/js/communs.js
@@ -0,0 +1,29 @@
+function writeHTMLNav() {
+    var htmlCode = `
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="/">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/files/{{defaultFile}}">Fichiers</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/login">Déconnexion</a></li>
+                </ul>
+            </div>
+        </nav>
+    `;
+    document.write(htmlCode);
+}
+
+
+
+
+
+// Importer les éléments communs dans le fichier HTML
+// Pour importer ces éléments dans un fichier HTML, vous devez inclure le fichier JavaScript contenant cette fonction dans votre balise <script>.
+// Assurez-vous d'inclure le chemin correct vers le fichier JavaScript dans l'attribut src de la balise <script>.
+// Par exemple, si votre fichier JavaScript est situé à "/static/js/communs.js", vous pouvez inclure le code suivant dans votre fichier HTML :
+
+// <script src="/static/js/communs.js"></script>
+// Vous pouvez placer cette balise <script> à la fin du corps de votre fichier HTML pour vous assurer que le DOM est entièrement chargé avant d'exécuter le code JavaScript.
