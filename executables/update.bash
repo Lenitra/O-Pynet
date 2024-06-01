79f012fc4356f2acc18439416d77cdd352ef9857
Lenitra <lenitramc@gmail.com>
2024-06-01 17:04:06 +0200

    Refactor update script to rename the file for O-Pynet

--- executables/update.bash
+++ executables/update.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Chemin vers le répertoire où se trouve ce script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 # Déplace le fichier de configuration
-mv -f O-Pynet/config.json tempopyconfig.json
+mv -f $SCRIPT_DIR/O-Pynet/config.json $SCRIPT_DIR/tempopyconfig.json
 
 # Supprime le dossier O-Pynet
-rm -rf O-Pynet/
+rm -rf $SCRIPT_DIR/O-Pynet/
 
 # Clone le dépôt
-git clone https://github.com/Lenitra/O-Pynet.git
+git clone https://github.com/Lenitra/O-Pynet.git $SCRIPT_DIR/O-Pynet
 
 # Remet le fichier de configuration
-mv -f tempopyconfig.json O-Pynet/config.json
-
+mv -f $SCRIPT_DIR/tempopyconfig.json $SCRIPT_DIR/O-Pynet/config.json
 
 # Remet le fichier de mise à jour
-mv -f O-Pynet/executables/update.bash updateOpynet.bash
+mv -f $SCRIPT_DIR/O-Pynet/executables/update.bash $SCRIPT_DIR/updateOpynet.bash
