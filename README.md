7936002bba61cabdf211db86e121be2b3e5e6544
TauMah <lenitramc@gmail.com>
2024-06-18 14:37:41 +0200

    chore: Clean up code and remove unnecessary comments

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # O-Pynet
-Panneau administratif pour serveur vps ou dédié
+Panneau administratif pour serveur personnel
 
 ### Fonctionnalités
 - Authentification 
 - Affichage des ressources utilisées (CPU, RAM, Stockage)
 - Bouton de redémarrage rapide
-- Boutons vers liens externes
-- Navigation et gestion des fichiers (TODO)
+- Visualisation des fichiers
+- Connexion à l'api spotify
+- Camera de surveillance (experimental)
 
 ### Installation
-1. Cloner le dépôt dans un dossier
-2. Déplacez update.bat et start.bat dans le répertoire parent du clone
-3. Modifiez les config.json, pour l'os vous pouvez mettre "windows" ou "linux"
-4. Dans le répertoire parent du clone, vous aurez un start.bat à executer pour lancer le serveur
-
-⛔⛔⛔ ne pas bouger le start.bat et le update.bat de leur emplacement ⛔⛔⛔
-
-Les mises à jour se font automatiquement. Pour les forcer, executez update.bat dans le répertoire parent du clone.
-
+Démerdez vous, je ferai un truc clean plus tard
--- app/routes/cam.py
+++ app/routes/cam.py
@@ -1,24 +1,29 @@
-
 from flask import Blueprint, session, redirect, render_template
 import cv2
 import os
 
 
-camera = cv2.VideoCapture(0)
-    
-    
 CAM = Blueprint('cam', __name__)
 
+
 @CAM.route('/cam')
-def spotify():
-    if 'user' not in session:
-        session['redirect'] = 'cam'
+def camera():
+    if "user" not in session:
+        session["redirect"] = "cam"
         return redirect("/login")
-    success, frame = camera.read()
+
+    # Assurez-vous que la caméra est correctement initialisée
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        return redirect("/cam")
+
+    success, frame = cap.read()
+    cap.release()  # Libérez la caméra après utilisation
+
     if success:
         # Enregistrer l'image capturée
-        img_path = 'app/static/captured_image.jpg'
+        img_path = "app/static/captured_image.jpg"
         cv2.imwrite(img_path, frame)
-        return render_template('cam.html', img_path=img_path)
-    
-    
+        return render_template("cam.html")
+    else:
+        return redirect("/cam")
