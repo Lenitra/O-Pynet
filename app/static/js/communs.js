5931fd72f40bf8e6ce8c0a76ede1068f1419c353
TauMah <lenitramc@gmail.com>
2024-07-21 09:54:30 +0200

    Fin des configs : - Raccourcis de dossiers - Gestion des utilisateurs

--- app/static/js/communs.js
+++ app/static/js/communs.js
@@ -15,7 +15,7 @@
         <nav class="navbar navbar-dark bg-dark">
             <div class="navbar-nav">
                 <a class="nav-item nav-link" href="/"><i class="fas fa-home"></i></a>
-        <a class="nav-item nav-link" href="/files"><i class="fas fa-file-alt"></i></a><a class="nav-item nav-link" href="/cam"><i class="fas fa-camera"></i></a>
+        
                 <a class="nav-item nav-link" href="/config"><i class="fas fa-cog"></i></a>
                 <a class="nav-item nav-link" href="/login"><i class="fas fa-sign-out-alt"></i></a>
             </div>
